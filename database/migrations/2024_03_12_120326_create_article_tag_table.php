e2086f0a206df786c790fb1e4a0d010dada3a94d
rajat <rajat.k@sphinxworldbiz.co>
2024-03-15 13:17:19 +0530

    Created models and migrations for article, tags, followers and admin tables

--- database/migrations/2024_03_12_120326_create_article_tag_table.php
+++ database/migrations/2024_03_12_120326_create_article_tag_table.php
@@ -11,15 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('article_tag', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('image')->nullable();
-            $table->longText('bio')->nullable();
-            $table->rememberToken();
+            $table->foreignId('article_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -29,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('article_tag');
     }
 };
