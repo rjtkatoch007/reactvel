a06120b80f3c8586f8f1f8b84e2763f989f67e29
rajat <rajat.k@sphinxworldbiz.co>
2024-03-27 16:52:40 +0530

    AdminController created, defined admin guard and made AuthAdminRequest custom request

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
+Route::post('auth', [AdminController::class, 'auth'])->name('admin.auth');
+
+Route::prefix('admin')->group(function(){
+    Route::get('dashboard', [AdminController::class, 'index'])->name('admin.index');
 });
