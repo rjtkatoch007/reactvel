3f453d595a609461fda28f2ec91a65559fef247c
rajat <rajat.k@sphinxworldbiz.co>
2024-04-02 17:19:33 +0530

    Article routes Done in api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    //articles routes
+    Route::get('followers/articles', [ArticleController::class, 'fetchFollowingArticles']);
+    Route::post('add/articles', [ArticleController::class, 'store']);
+    Route::put('update/{article}/articles', [ArticleController::class, 'update']);
+    Route::delete('delete/{article}/articles', [ArticleController::class, 'delete']);
+    Route::get('clap/{article}/articles', [ArticleController::class, 'articleClap']);
+        
 });
+
+//articles routes
+Route::get('articles', [ArticleController::class, 'index']);
+Route::get('articles/{article}', [ArticleController::class, 'show']);
+Route::post('find/articles', [ArticleController::class, 'fetchByTerm']);
+Route::get('tag/{tag}/articles', [ArticleController::class, 'fetchByTag']);
